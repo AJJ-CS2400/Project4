64912fffe5691e525ffa1c043075f6655e793862
JustinHaCPP <justinh@cpp.edu>
2022-04-12 09:52:56 -0700

    init

---
+++ MaxHeapArray.java
@@ -0,0 +1,72 @@
+import java.util.Arrays;
+
+public final class MaxHeapArray<T extends Comparable <? super T>> implements MaxHeapInterface<T>
+{
+    private T[] heap;       // Array of heap entries
+    private int lastIndex;  // Index of last entry
+    private boolean initialized = false;
+    private static final int DEFAULT_CAPACITY = 25;
+    private static final int MAX_CAPACITY = 10000;
+
+    public MaxHeapArray()
+    {
+        this(DEFAULT_CAPACITY); // Call next constructor
+    } // end default constructor
+
+    public MaxHeapArray(int initialCapacity)
+    {
+        // Is initialCapacity too small?
+        if (initialCapacity < DEFAULT_CAPACITY)
+            initialCapacity = DEFAULT_CAPACITY;
+        else // Is initialCapacity too big?
+            checkCapacity(initialCapacity);
+
+        // The cast is safe because the new array contains all null entries
+        @SuppressWarnings("unchecked")
+        T[] tempHeap = (T[]) new Comparable[initialCapacity + 1];
+        heap = tempHeap;
+        lastIndex = 0;
+        initialized = true;
+    } // end constructor
+
+    public void add(T newEntry)
+    {
+    } // end add
+
+    public T removeMax()
+    {
+        return null; // stub
+    } // end removeMax
+
+    public T getMax()
+    {
+        return null;
+    } // end getMax
+
+    public boolean isEmpty()
+    {
+        return false; // stub
+    } // end isEmpty
+
+    public int getSize()
+    {
+        return 0; // stub
+    } // end getSize
+
+    public void clear()
+    {
+    } // end clear
+
+    // Throws an exception if the client requests a capacity that is too large.
+    private void checkCapacity(int capacity) {
+        if (capacity > MAX_CAPACITY)
+            throw new IllegalStateException("Attempt to create a bag whose capacity exceeds " +
+                    "allowed maximum of " + MAX_CAPACITY);
+    } // end checkCapacity
+
+    // Throws an exception if receiving object is not initialized.
+    private void checkInitialized() {
+        if (!initialized)
+            throw new SecurityException("ArrayBag object is corrupt.");
+    } // end checkIntegrity
+} // end MaxHeapArray
