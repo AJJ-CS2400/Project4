b487c96ad41b918adc9339b1bb23eaf3f95d890b
JustinHaCPP <justinh@cpp.edu>
2022-04-12 10:04:05 -0700

    implemented removeMox()

--- MaxHeapArray.java
+++ MaxHeapArray.java
@@ -35,7 +35,17 @@ public final class MaxHeapArray<T extends Comparable <? super T>> implements Max
 
     public T removeMax()
     {
-        return null; // stub
+        checkInitialization();
+        T root = null;
+
+        if (!isEmpty())
+        {
+            root = heap[1];
+            heap[1] = heap[lastIndex];
+            lastIndex--;
+            reheap(1);
+        } // end if
+        return root;
     } // end removeMax
 
     public T getMax()
@@ -67,6 +77,32 @@ public final class MaxHeapArray<T extends Comparable <? super T>> implements Max
         } // end while
         lastIndex = 0;
     } // end clear
+
+    private void reheap(int rootIndex)
+    {
+        boolean done = false;
+        T orphan = heap[rootIndex];
+        int leftChildIndex = 2 * rootIndex;
+
+        while (!done && (leftChildIndex <= lastIndex))
+        {
+            int largerChildIndex = leftChildIndex; // Assume larger
+            int rightChildIndex = leftChildIndex + 1;
+            if ((rightChildIndex <= lastIndex) && heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
+            {
+                largerChildIndex = rightChildIndex;
+            } // end if
+            if (orphan.compareTo(heap[largerChildIndex]) < 0)
+            {
+                heap[rootIndex] = heap[largerChildIndex];
+                rootIndex = largerChildIndex;
+                leftChildIndex = 2 * rootIndex;
+            }
+            else
+                done = true;
+        } // end while
+        heap[rootIndex] = orphan;
+    } // end reheap
 
     // Throws an exception if the client requests a capacity that is too large.
     private void checkCapacity(int capacity) {
